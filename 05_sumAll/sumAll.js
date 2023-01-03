e7504ba61c11941fdfceff301700d9d46885d354
hagrace4 <hagrace4@gmail.com>
2023-01-03 11:45:54 -0800

    Finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  let sum = 0;
 
+  if ((numOne < 0 || numTwo < 0)) {
+    return "ERROR";
+  }
+  if (typeof numOne !== 'number' || typeof numTwo !== 'number'){
+    return "ERROR";
+  }
+   else {
+    for (let i = Math.min(numOne, numTwo); i <= Math.max(numOne, numTwo); i++) {
+      sum += i;
+    }
+  }
+  return finalSum = sum;
 };
 
 // Do not edit below this line
