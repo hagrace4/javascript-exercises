4310162f7790a977e3f373095ec7dd7144739f98
hagrace4 <hagrace4@gmail.com>
2023-01-03 08:14:53 -0800

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
 const removeFromArray = function(arr, ...args) {
+    // Will only work with 2 arguments
+    // arr = arr.filter(el => el !== args); 
 
-    arr = arr.filter(el => el !== args);
+    arr = arr.filter(el => !args.includes(el));
 
     newArray = arr;
     return newArray;
