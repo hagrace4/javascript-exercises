614560363d36cc8f2a9abb9ec78c2380c6003fa7
hagrace4 <hagrace4@gmail.com>
2023-01-03 17:58:46 -0800

    Complete exercise 6: Leap Years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let yearDiv4 = year % 4;
+  let yearDiv100 = year % 100;
+  let yearDiv400 = year % 400;
+
+  if (yearDiv400 === 0) {
+    return true;
+  }
+  if (yearDiv100 == 0) {
+    return false;
+  }
+  if (yearDiv4 == 0 ) {
+    return true;
+  }
+  return false;
+
+  // return Boolean;
 
 };
 
