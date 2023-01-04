52e2af8b0d877e2e7063a5b64daea421628d638f
hagrace4 <hagrace4@gmail.com>
2023-01-03 19:10:30 -0800

    Finish exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// F = C * 1.8 + 32
+const convertToCelsius = function(fVal) {
+  let cVal = (fVal -32) / 1.8;
+  cVal = cVal.toFixed(1);
+  return Number(cVal);
 };
 
-const convertToFahrenheit = function() {
+// C = (F - 32) / 1.8
+const convertToFahrenheit = function(cVal) {
+  let fVal = ((cVal*1.8) +32);
+  fVal = fVal.toFixed(1);
+  return Number(fVal);
 };
 
 // Do not edit below this line
