4be0601dee98542e419f031479ab111db1c4d705
hagrace4 <hagrace4@gmail.com>
2023-01-04 16:26:51 -0800

    Finish exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0){
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= n; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
+
+
 
 };
 
